26acb07ec937586292ab5e7b7df879cbcb1a09e4
Joseph Sobrero <joesobrero@gmail.com>
2025-04-15 21:19:58 -0700

    toast-stack-wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={cn(
+        geistMono.variable,
+        geistSans.variable,
+        "antialiased scheme-light dark:scheme-dark"
+      )}
+    >
+      <body className="bg-base-100 dark:bg-base-100-dark">{children}</body>
     </html>
   );
 }
